3a6d33f51cb40d012c51e547f3e645335499c592
dantleech <dan.t.leech@gmail.com>
2016-07-02 09:58:23 +0100

    Added movable js

--- src/Resources/private/js/column_browser.js
+++ src/Resources/private/js/column_browser.js
@@ -1,6 +1,22 @@
 jQuery.fn.sycmsColumnBrowser = function () {
+    var operations = [];
+
     $('.sortable').sortable({
-        'handle': '.grippy'
+        'stop': function (event) {
+            console.log(event);
+        }
     });
     $('.columnbrowser').disableSelection();
+    $('.moving').hide();
+
+    $('#browser_move_button').on('click', function (el) {
+        $('.browsing').hide();
+        $('.moving').show();
+    });
+
+    $('#moving_cancel_button').on('click', function (el) {
+        $('.moving').hide();
+        $('.browsing').show();
+        $('.sortable').sortable('cancel');
+    });
 };
